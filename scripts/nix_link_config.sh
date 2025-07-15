65153ca68393913e45c885103a25457f2b267e04
keiwop <keiwop.dev@gmail.com>
2025-07-15 16:32:07 +0200

    added folding protection to stow

--- scripts/nix_link_config.sh
+++ scripts/nix_link_config.sh
@@ -8,17 +8,19 @@ CFG_DIR=/_/etc/nixos
 DOT_DIR=${CFG_DIR}/dotfiles
 NIX_DIR=${CFG_DIR}/nix
 
-echo -e "\n--------------------------------------------------------------------------------"
-echo -e "nix_init: Linking configurations files where they are needed"
+echo -e "\n--------------------------------------------------------------------------------\n"
+echo -e "nix_init: Linking nixos configuration"
 
 if [[ -d $NIX_DIR ]]; then
     echo "🔗 Stowing $NIX_DIR"
-    stow --restow --dir="$NIX_DIR" --target=/etc/nixos/ .
+    stow --restow --no-folding --dir="$NIX_DIR" --target=/etc/nixos/ .
 fi
+
+echo -e "\nnix_init: Linking dotfiles"
 
 if [[ -d $DOT_DIR ]]; then
     echo "🔗 Stowing $DOT_DIR"
-    sudo -u $USER_NAME stow --restow --dotfiles --dir="$DOT_DIR" --target=/home/$USER_NAME/ .
+    sudo -u $USER_NAME stow --restow --no-folding --dotfiles --dir="$DOT_DIR" --target=/home/$USER_NAME/ .
 fi
 
 echo -e "\n--------------------------------------------------------------------------------\n"

8d7783f63b6e8662c7ba6656dd522f0c020590af
blacktoz <34444000+blacktoz@users.noreply.github.com>
2026-01-04 20:07:55 +0100

    Update build_link_config_files.sh

--- scripts/build_link_config_files.sh
+++ scripts/build_link_config_files.sh
@@ -4,4 +4,4 @@ set -e
 
 nix-build /_/etc/nixos/nix/scripts/link_config_files.nix \
   --arg pkgs 'import <nixpkgs> {}' \
-  --arg linked_paths '(import /_/etc/nixos/nix/paths.nix { user_name = "keiwop"; machine_config = import /_/etc/nixos/nix/machine_config/nix-thinkpad.nix { pkgs = import <nixpkgs> {}; user_name = "keiwop"; }; }).linked_paths'
+  --arg linked_paths '(import /_/etc/nixos/nix/paths.nix { user_name = "ryo"; machine_config = import /_/etc/nixos/nix/machine_config/nix-ryo.nix { pkgs = import <nixpkgs> {}; user_name = "ryo"; }; }).linked_paths'
